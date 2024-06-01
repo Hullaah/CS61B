f67b0c9c25ebe558906962b431a5b47bfd6566cf
Hullaah <umaradelo1.247@gmail.com>
2024-06-01 14:08:40 +0100

    Implemented helper methods for the Model of the game. Remains to implement the tilt method which is the main logic of the game

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author Umar Adelowo
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,9 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for (var tile: b)
+            if (tile == null)
+                return true;
         return false;
     }
 
@@ -147,7 +149,9 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        for (var tile: b)
+            if (tile != null && tile.value() == MAX_PIECE)
+                return true;
         return false;
     }
 
@@ -158,7 +162,27 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        if (emptySpaceExists(b)) return true;
+        for (int row = 0; row < b.size(); row++) {
+            for (int col = 0; col < b.size(); col++) {
+                if (row < b.size() - 1) {
+                    if (b.tile(col, row).value() == b.tile(col, row+1).value())
+                        return true;
+                }
+                if (row > 0) {
+                    if (b.tile(col, row).value() == b.tile(col, row-1).value())
+                        return true;
+                }
+                if (col < b.size() - 1) {
+                    if (b.tile(col, row).value() == b.tile(col+1, row).value())
+                        return true;
+                }
+                if (col > 0) {
+                    if (b.tile(col, row).value() == b.tile(col-1, row).value())
+                        return true;
+                }
+            }
+        }
         return false;
     }
 
